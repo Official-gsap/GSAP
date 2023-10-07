6e88b6d2f4e8446573bc43b5bce39f11a4d02479
Official-gsap <146415896+Official-gsap@users.noreply.github.com>
2023-10-07 03:46:25 +0000

    desciption

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
   name: "GSAP",
-  description: "GSAP is a clothing brand started by college students who sells unique designs and prints for you",
+  description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
